f8db477d98739cf596156e02a638a9833656a47d
janvi-scaletech <janvi.kalavadiya@scaletech.xyz>
2025-06-03 02:45:18 +0530

    remove unsed code

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaHome, FaMusic, FaUser, FaSignOutAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
